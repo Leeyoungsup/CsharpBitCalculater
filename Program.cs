bb2f9c1306f7ecfd5ccfae08420d06e1ea2a8326
leeyoungsub <liive29252@gmail.com>
2020-06-04 18:46:51 +0900

    200604

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BitCalculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint num = 1;
+            // << >> &(and) |(or) ^(xor) ~(not)
+            num = num << 3;//0001-> 1000
+            num = num >> 1;//1000 -> 0100
+            uint num1 = 15;//1111
+            uint num2 = 6;//0110
+            num = num1 & num2;//0110
+            num = num1 | num2;//1111
+            num = num1 ^ num2;//1001
+            num = ~num1;//1111111111110000
+            Console.WriteLine(num);
+            
+        }
+    }
+}
